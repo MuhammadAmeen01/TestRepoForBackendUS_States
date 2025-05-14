d54f2b15203adb8c04e76a049c01fdd259700e42
Noah S <n_solomon@mail.fhsu.edu>
2025-05-14 04:49:26 -0700

    Created container 1

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ app.use(express.json());
 app.use('/', require('./routes/root'));
 app.use('/states', require('./routes/states'));
 
+// Root endpoint returns HTML
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  });
+
 mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB');
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
