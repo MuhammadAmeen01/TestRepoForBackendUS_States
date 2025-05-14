d0a291eb598c969b3fde899e7c5977c5f8f5e718
Noah S <n_solomon@mail.fhsu.edu>
2025-05-14 16:57:18 -0700

    Created container 17

---
+++ middleware/validateState.js
@@ -0,0 +1,18 @@
+const validStates = [
+    'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA',
+    'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD',
+    'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ',
+    'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
+    'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'
+  ];
+  
+  function validateStateParam(req, res, next) {
+    const state = req.params.state?.toUpperCase();
+    if (!validStates.includes(state)) {
+      return res.status(404).json({ message: 'Invalid state abbreviation parameter' });
+    }
+    next();
+  }
+  
+  module.exports = validateStateParam;
+  
--- routes/states.js
+++ routes/states.js
@@ -1,32 +1,37 @@
 const express = require('express');
 const router = express.Router();
 const statesController = require('../controllers/statesController');
+const validateStateParam = require('../middleware/validateState');
 
 // General state routes
 router.route('/')
     .get(statesController.getAllStates);
 
 router.route('/:state')
+    .all(validateStateParam)
     .get(statesController.getState);
 
-// Fun Facts routes
 router.route('/:state/funfact')
+    .all(validateStateParam)
     .get(statesController.getFunFact)
     .post(statesController.addFunFacts)
     .patch(statesController.updateFunFact)
     .delete(statesController.deleteFunFact);
 
-// Specific state info routes
 router.route('/:state/capital')
+    .all(validateStateParam)
     .get(statesController.getCapital);
 
 router.route('/:state/nickname')
+    .all(validateStateParam)
     .get(statesController.getNickname);
 
 router.route('/:state/population')
+    .all(validateStateParam)
     .get(statesController.getPopulation);
 
 router.route('/:state/admission')
+    .all(validateStateParam)
     .get(statesController.getAdmission);
 
 module.exports = router;
