c041fb019dbd8c0d6d92e1578f3ead89ca28e0c7
Noah S <n_solomon@mail.fhsu.edu>
2025-05-14 20:26:43 -0700

    Created container 06

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,9 +1,17 @@
-const { logEvents } = require('./logEvents');
+// const { logEvents } = require('./logEvents');
+
+// const errorHandler = (err, req, res, next) => {
+// 	logEvents(`${err.name}: ${err.message}`, 'errlog.txt');
+//         console.error(err.stack)
+// 	res.status(500).send(err.message);
+// }
+
+// module.exports = errorHandler;
+
 
 const errorHandler = (err, req, res, next) => {
-	logEvents(`${err.name}: ${err.message}`, 'errlog.txt');
-        console.error(err.stack)
-	res.status(500).send(err.message);
+	console.error(err.stack)
+	res.status(500).send(err.message)
 }
 
-module.exports = errorHandler;
+module.exports = errorHandler
--- routes/states.js
+++ routes/states.js
@@ -1,37 +1,72 @@
+// const express = require('express');
+// const router = express.Router();
+// const statesController = require('../controllers/statesController');
+// //const validateStateParam = require('../middleware/validateState');
+
+// // General state routes
+// router.route('/')
+//   .get(statesController.getAllStates)
+//   .post(statesController.createState);
+
+// router.route('/:state')
+//     .all(validateStateParam)
+//     .get(statesController.getState);
+
+// router.route('/:state/funfact')
+//     .all(validateStateParam)
+//     .get(statesController.getFunFact)
+//     .post(statesController.addFunFacts)
+//     .patch(statesController.updateFunFact)
+//     .delete(statesController.deleteFunFact);
+
+// router.route('/:state/capital')
+//     .all(validateStateParam)
+//     .get(statesController.getCapital);
+
+// router.route('/:state/nickname')
+//     .all(validateStateParam)
+//     .get(statesController.getNickname);
+
+// router.route('/:state/population')
+//     .all(validateStateParam)
+//     .get(statesController.getPopulation);
+
+// router.route('/:state/admission')
+//     .all(validateStateParam)
+//     .get(statesController.getAdmission);
+
+// module.exports = router;
+
 const express = require('express');
 const router = express.Router();
 const statesController = require('../controllers/statesController');
-const validateStateParam = require('../middleware/validateState');
 
-// General state routes
+// Get all states or filter by contig=true/false
 router.route('/')
-    .get(statesController.getAllStates);
+  .get(statesController.getAllStates);
 
+// Get a specific state by abbreviation (e.g. /states/CA)
 router.route('/:state')
-    .all(validateStateParam)
-    .get(statesController.getState);
+  .get(statesController.getState);
 
+// Fun facts for a specific state
 router.route('/:state/funfact')
-    .all(validateStateParam)
-    .get(statesController.getFunFact)
-    .post(statesController.addFunFacts)
-    .patch(statesController.updateFunFact)
-    .delete(statesController.deleteFunFact);
+  .get(statesController.getFunFact)
+  .post(statesController.addFunFacts)
+  .patch(statesController.updateFunFact)
+  .delete(statesController.deleteFunFact);
 
+// Specific attributes for a state
 router.route('/:state/capital')
-    .all(validateStateParam)
-    .get(statesController.getCapital);
+  .get(statesController.getCapital);
 
 router.route('/:state/nickname')
-    .all(validateStateParam)
-    .get(statesController.getNickname);
+  .get(statesController.getNickname);
 
 router.route('/:state/population')
-    .all(validateStateParam)
-    .get(statesController.getPopulation);
+  .get(statesController.getPopulation);
 
 router.route('/:state/admission')
-    .all(validateStateParam)
-    .get(statesController.getAdmission);
+  .get(statesController.getAdmission);
 
 module.exports = router;
