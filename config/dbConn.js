84d6e9cf5cfa484b17dca6052637df82609fbfcd
Noah S <n_solomon@mail.fhsu.edu>
2025-05-14 19:47:13 -0700

    Created container 04

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,14 +1,26 @@
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect(process.env.DATABASE_URI, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true
+//         });
+//     } catch (err) {
+//         console.error(err);
+//     }
+// };
+
+// module.exports = connectDB;
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.DATABASE_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-    } catch (err) {
-        console.error(err);
-    }
+  try {
+    await mongoose.connect(process.env.DATABASE_URI);
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 module.exports = connectDB;
