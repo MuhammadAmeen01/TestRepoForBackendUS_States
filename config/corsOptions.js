d283d90b91377a237b0c4a1cce9353cfb5f5d43d
Noah S <n_solomon@mail.fhsu.edu>
2025-05-16 05:23:58 -0700

    Created container 04

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,18 +1,18 @@
-const allowedOrigins = [
-    'https://dazzling-snickerdoodle-777101.netlify.app/',
-    'http://127.0.0.1:5500',
-    'http://localhost:3500'
-];
+const whitelist = [
+	'https://dazzling-snickerdoodle-777101.netlify.app',
+	'http://127.0.0.1:5500',
+	'http://localhost:3500'
+]
 
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    optionsSuccessStatus: 200
+	origin: (origin, callback) => {
+		if (whitelist.indexOf(origin) !== -1 || !origin) {
+			callback(null, true)
+		} else {
+			callback(new Error('Not allowed by CORS'))
+		}
+	},
+	optionsSuccessStatus: 200
 }
 
-module.exports = corsOptions;
+module.exports = corsOptions
