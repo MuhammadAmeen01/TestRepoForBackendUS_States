d52189385baedd8c67e5bb9c3271ac50344cda76
Noah S <n_solomon@mail.fhsu.edu>
2025-05-16 04:41:06 -0700

    Created container 01

--- server.js
+++ server.js
@@ -13,8 +13,7 @@ const PORT = process.env.PORT || 3500;
 
 connectDB(); // Connect to MongoDB
 
-app.use(cors(corsOptions));
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
